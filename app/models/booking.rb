ca9b605fecc15c33e65294cdb3faaac8bad07c50
jonathan.mallafre05 <jonathan.mallafre05@gmail.com>
2024-03-12 12:45:28 +0100

    models validates ok

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :cars
   belongs_to :users
+  validates :status, presence: true
+  validates :bookings, presence: true
+  validates :starting_date, presence: true
+  validates :ending_date, presence: true
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,8 @@
 class Car < ApplicationRecord
   belongs_to :users
   has_many :bookings
+  validates :car_model, presence: true
+  validates :car_brand, presence: true
+  validates :price, presence: true
+  validates :horse_power, presence: true
 end
